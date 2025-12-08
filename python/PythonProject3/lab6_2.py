de62d509fe9661e965fabfd669082427068bf6a1
Максименко Николай <maksimenko_nd_25@student.grsu.by>
2025-12-08 21:28:26 +0300

    задание 2 доделано

--- python/PythonProject3/lab6_2.py
+++ python/PythonProject3/lab6_2.py
@@ -85,8 +85,85 @@ def analyze_user_activity(users):
         print('')
     return
 
+
+def  analyze_workout_types(workouts):
+    workouts_stat = {}
+    print("РАСПРЕДЕЛЕНИЕ ПО ТИПАМ ТРЕНИРОВОК:")
+    for w in workouts:
+        if w['type'] not in workouts_stat:
+            workouts_stat.update([(w['type'], [])])
+        workouts_stat[w['type']].append(w)
+
+    for key in workouts_stat:
+        a = workouts_stat[key]
+        n = 0
+        d = 0
+        c = 0
+        for w in a:
+            n = n + 1
+            d = d + w['duration']
+            c = c + w['calories']
+        print(key.capitalize() + ':', n, 'тренировок', '(', round(n*100/len(workouts), 1), '%)')
+        print('  Средняя длительность:', round(d/n, 0), 'мин')
+        print('  Средние калории:', round(c/n, 0), 'ккал')
+    return
+
+def  find_user_workouts(users, user_name):
+    for u in users:
+        if u['name'] == user_name:
+            return u['workouts']
+    print("пользователь не найден")
+    return []
+
+def find_user(users, user_name):
+    for u in users:
+        if u['name'] == user_name:
+            return u
+    print("пользователь не найден")
+    return []
+
+def analyze_user(user, workouts):
+    print('ДЕТАЛЬНЫЙ АНАЛИЗ ДЛЯ ПОЛЬЗОВАТЕЛЯ:', user['name'])
+    print('=============================================')
+    print('Возраст:', user['age'], 'лет,', 'Вес:', user['weight'],'кг')
+    print('уровень:', user['fitness_level'])
+
+    n = 0
+    c = 0
+    t = 0
+    dist = 0
+    workouts_stat = {}
+    for w in user['workouts']:
+        if w['type'] not in workouts_stat:
+            workouts_stat.update([(w['type'], [])])
+        workouts_stat[w['type']].append(w)
+        n = n +1
+        c = c + w['calories']
+        t = t + w['duration']
+        dist = dist + w['distance']
+    max1 = 0
+    key_max =' '
+    for key in workouts_stat:
+        a = workouts_stat[key]
+        if len(a) > max1:
+            key_max = key
+            max1 = len(a)
+
+
+
+    print('Тренировок:',n )
+    print('Сожжено калорий:', c)
+    print('Общее время:', round(t/60, 1), 'часов')
+    print('Пройдено дистанции:', round(dist, 1), 'км')
+    print('Средние калории за тренировку:', round(c/n, 0))
+    print('Любимый тип тренировки:', key_max)
+
+
 u = load_users_data()
 w = load_workouts_data()
 populate_user_worcouts(u,w)
 
 analyze_user_activity(u)
+analyze_workout_types(w)
+find_user_workouts(u, 'Борис')
+analyze_user(find_user(u, 'Борис'), w)
